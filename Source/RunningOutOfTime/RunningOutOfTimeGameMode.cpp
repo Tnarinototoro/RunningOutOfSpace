4f3609f41fd52a3a3a34a2c2777b5882b4d9f29a
Yorda <921864047@qq.com>
2021-08-29 12:03:55 +0900

    修复CPP报错

--- Source/RunningOutOfTime/RunningOutOfTimeGameMode.cpp
+++ Source/RunningOutOfTime/RunningOutOfTimeGameMode.cpp
@@ -9,9 +9,9 @@ ARunningOutOfTimeGameMode::ARunningOutOfTimeGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	//DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
-	HUDClass = ARunningOutOfTimeHUD::StaticClass();
+	//HUDClass = ARunningOutOfTimeHUD::StaticClass();
 }
